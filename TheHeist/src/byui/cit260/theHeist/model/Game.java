8d38421b8139747544d0866671e44c4538eb6fbb
luceromoreno <luceromoreno@10.18.151.6>
2016-01-29 09:45:20 -0700

    01/27/2016-Initial

---
+++ TheHeist/src/byui/cit260/theHeist/model/Game.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHeist.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author luceromoreno
+ */
+public class Game implements Serializable{
+
+    
+    private String score;
+    private double startTime;
+
+    public Game() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 79 * hash + Objects.hashCode(this.score);
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.startTime) ^ (Double.doubleToLongBits(this.startTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "score=" + score + ", startTime=" + startTime + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.startTime) != Double.doubleToLongBits(other.startTime)) {
+            return false;
+        }
+        if (!Objects.equals(this.score, other.score)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public double getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(double startTime) {
+        this.startTime = startTime;
+    }
+
+
+
+}
