e51577b1a60dcf1ac569ad76a7378c4a9b8cae9c
wr <wr@10.18.151.135>
2016-02-11 09:35:22 -0700

    Merge origin/master Test cases for group equation
    Conflicts:
    	TheHeist/src/theheist/TheHeist.java

---
+++ TheHeist/src/theheist/control/ClueControl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theheist.control;
+
+/**
+ *
+ * @author wr
+ */
+public class ClueControl {
+    public double getCombination(double clueA, double clueB, double clueC){
+        if (clueA <= 0 || clueA > 25){
+            return -1;
+        }    
+                
+        if (clueB <= 0 || clueB > 25){
+            return -1;
+        }    
+                
+        if (clueC <= 0 || clueC > 25){
+            return -1;
+        }    
+            
+        double combo = (clueA * clueB + clueC) / clueA;
+            return combo;    
+    }
+}
