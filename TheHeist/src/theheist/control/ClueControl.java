4dbb05aaec71c67e492ca3456ee259dfe4b0ff01
wr <wr@10.18.151.135>
2016-02-12 10:14:36 -0700

    addressClue function and tests done

--- TheHeist/src/theheist/control/ClueControl.java
+++ TheHeist/src/theheist/control/ClueControl.java
@@ -26,4 +26,24 @@ public class ClueControl {
         double combo = (clueA * clueB + clueC) / clueA;
             return combo;    
     }
+
+
+    public double addressClue(double clueA, double clueB, double clueC) {
+        if (clueA <= 0 || clueA > 25) {
+            return -1;
+        }        
+
+        if (clueB <= 0 || clueB > 25) {
+            return -1;
+        }
+
+        if (clueC <= 0 || clueC > 25) { 
+            return -1;
+        }
+
+        double addressNumber = Math.pow(clueA + clueB, 2) * clueC;
+            return addressNumber;
+
+    }
+    
 }
