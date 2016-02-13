36d55a00b2d969f8b49c10ba8214ad68ad980d15
luceromoreno <luceromoreno@10.18.151.6>
2016-02-13 12:27:08 -0700

    01/30/2016-Initial

--- TheHeist/src/theheist/control/ClueControl.java
+++ TheHeist/src/theheist/control/ClueControl.java
@@ -10,6 +10,24 @@ package theheist.control;
  * @author wr
  */
 public class ClueControl {
+    
+    public double getJewelAmount(double diamonds, double rubies, double sapphires){
+      if (diamonds <=0  || diamonds > 8){
+	return -1;
+                }
+      if (rubies  <=0 || rubies > 8){
+	return -1;
+        }
+      if (sapphires <= 0 || sapphires > 8){
+	return -1;
+      }
+      
+ 
+      double worth = (int) ((7 * diamonds + 4 * rubies + 4 * sapphires) - (4 * sapphires));
+      return worth;
+    }
+
+
     public double getCombination(double clueA, double clueB, double clueC){
         if (clueA <= 0 || clueA > 25){
             return -1;
