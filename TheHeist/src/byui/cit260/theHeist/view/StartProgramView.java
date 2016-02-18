a5f74e3293b6b0facdb9a10d3cdadbcec93bd84d
luceromoreno <luceromoreno@10.18.151.6>
2016-02-18 10:34:31 -0700

    Merge origin/master
    Conflicts:
    	TheHeist/src/theheist/control/ClueControl.java
    	TheHeist/test/theheist/control/ClueControlTest.java

--- TheHeist/src/byui/cit260/theHeist/view/StartProgramView.java
+++ TheHeist/src/byui/cit260/theHeist/view/StartProgramView.java
@@ -5,61 +5,65 @@
  */
 package byui.cit260.theHeist.view;
 
+import byui.cit260.theHeist.model.Player;
 import java.util.Scanner;
+import theheist.control.GameControl;
 
 /**
  *
  * @author luceromoreno
  */
 public class StartProgramView {
+
     private String promptMessage;
 
-    public StartProgramView(){
+    public StartProgramView() {
         this.promptMessage = "\nPlease enter yout name: ";
-        
+
         this.displayBanner();
-        
+
     }
 
     public void displayBanner() {
         System.out.println(
-  "\n************************************************************************************"
-+ "\n* Welcome to the Heist! It’s your chance to help us solve the great mystery.       *"
-+ "\n* Someone has stolen diamonds from the Queen of Ingland and we cannot solve        *"
-+ "\n* the mystery without you. Help Max Zepher our number one detective by solving     *"
-+ "\n* some puzzles and riddles which help him get closer to the thief. Once you find   *"
-+ "\n* the thief’s name you will have accomplished the goal. Thank you for your help,   *"
-+ "\n* mate!                                                                            *"
-+ "\n***********************************************************************************"
-);
-        
+                "\n************************************************************************************"
+                + "\n* Welcome to the Heist! It’s your chance to help us solve the great mystery.       *"
+                + "\n* Someone has stolen diamonds from the Queen of Ingland and we cannot solve        *"
+                + "\n* the mystery without you. Help Max Zepher our number one detective by solving     *"
+                + "\n* some puzzles and riddles which help him get closer to the thief. Once you find   *"
+                + "\n* the thief’s name you will have accomplished the goal. Thank you for your help,   *"
+                + "\n* mate!                                                                            *"
+                + "\n***********************************************************************************"
+        );
+
     }
 
     public void displayStartProgramView() {
-        
+
         boolean done = false;
         do {
             String playerName = this.getPlayerName();
-            if (playerName.toUpperCase().equals("Q"))
+            if (playerName.toUpperCase().equals("Q")) {
                 return;
-            
+            }
+
             done = this.doAction(playerName);
-           
+
         } while (!done);
     }
 
     private String getPlayerName() {
-        
+
         Scanner keyboard = new Scanner(System.in);
         String value = "";
         boolean valid = false;
-        
+
         while (!valid) {
             System.out.println("\n" + this.promptMessage);
-            
+
             value = keyboard.nextLine();
             value = value.trim();
-            
+
             if (value.length() < 1) {
                 System.out.println("\nInvalid value: value can not be blank");
                 continue;
@@ -67,13 +71,30 @@ public class StartProgramView {
             break;
         }
         return value;
-   
+
     }
 
     private boolean doAction(String playerName) {
-    System.out.println("\n*** doAction() called***");
-    return true;
-    }
-    
-}
 
+        if (playerName.length() < 2) {
+            System.out.println("Invalid players name: "
+                    + "The name must be greater than one character in length");
+            return false;
+        }
+        Player player = GameControl.createPlaye(playerName);
+        if (player == null) {
+            System.out.println("\nError creating the player.");
+            return false;
+        }
+        this.displayNextView(player);
+        return true;
+
+    }
+ 
+    
+
+    private void displayNextView(Player player) {
+
+    }
+
+}
