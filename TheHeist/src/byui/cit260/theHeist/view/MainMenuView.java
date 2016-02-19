1e31e1ce485093c4a71b1801cc56ca367a09c209
luceromoreno <luceromoreno@10.18.151.6>
2016-02-19 11:27:35 -0700

    Merge origin/master
    Conflicts:
    	TheHeist/src/theheist/control/ClueControl.java
    	TheHeist/test/theheist/control/ClueControlTest.java

--- TheHeist/src/byui/cit260/theHeist/view/MainMenuView.java
+++ TheHeist/src/byui/cit260/theHeist/view/MainMenuView.java
@@ -5,6 +5,8 @@
  */
 package byui.cit260.theHeist.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author luceromoreno
@@ -12,6 +14,7 @@ package byui.cit260.theHeist.view;
 public class MainMenuView
 {
     private final String menu;
+    private String promptMessage;
     
     public MainMenuView() 
     {this.menu = "\n"
@@ -36,13 +39,63 @@ public class MainMenuView
     }
 
     private String getMenuOption() {
-    System.out.println("\n");   
-    return "G";
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println("\n" + this.promptMessage);
+
+            value = keyboard.nextLine();
+            value = value.trim();
+
+            if (value.length() < 1) {
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            break;
+        }
+        return value;
     }
 
-    private boolean doAction(String menuOption) {
-    System.out.println("\n");
-    return true;
+    public boolean doAction(String choice) {
+    
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+        case "G":
+        this.startNewGame();
+        break;
+        case "H":
+            this.getHelp();
+            break;
+        case "L":
+            this.loadGame();
+            break;
+        case "S":
+            this.saveGame();
+            break;
+        default:
+            System.out.println("\n*** Invalid selection*** Try again");
+            break;
+        }
+    return false;
+    }
+
+    private void startNewGame() {
+        System.out.println("*** startNewGame function called ***");
+    }
+
+    private void getHelp() {
+        System.out.println("*** getHelp function called ***");
+    }
+
+    private void loadGame() {
+        System.out.println("*** loadGame function called ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** saveGame function called ***");
     }
      }
     
