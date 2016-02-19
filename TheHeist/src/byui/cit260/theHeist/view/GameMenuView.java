8ecae92ba408e8ec15daed67c1f712eda2ca22ab
wr <wr@10.18.151.135>
2016-02-19 16:18:36 -0700

    addressClue function and tests done

---
+++ TheHeist/src/byui/cit260/theHeist/view/GameMenuView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHeist.view;
+
+import byui.cit260.theHeist.model.Player;
+
+/**
+ *
+ * @author wr
+ */
+public class GameMenuView {
+    
+    public static void createNewGame(Player player) {
+        System.out.println("/n*** createNewGame stub function called ***");
+    }
+
+    void displayMenu() {
+        System.out.println("/n*** displayMenu stub function called ***");
+    }
+    
+}
