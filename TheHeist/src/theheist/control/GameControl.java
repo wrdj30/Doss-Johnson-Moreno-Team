e4dbe080069b4abcfc43e80b1e3e17215cab64b3
wr <wr@10.18.151.135>
2016-02-19 21:45:37 -0700

    Had to fix getPlayer() function. Was put in the wrong class folder.

--- TheHeist/src/theheist/control/GameControl.java
+++ TheHeist/src/theheist/control/GameControl.java
@@ -12,6 +12,10 @@ import byui.cit260.theHeist.model.Player;
  * @author wr
  */
 public class GameControl {
+    
+    public static void createNewGame(Player player) {
+        System.out.println("/n*** createNewGame stub function called ***");
+    }
 
         public static Player createPlayer(String name) {
         if (name == null) {
