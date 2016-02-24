27b5817771242fd5b9565e7e1475dca7f305075f
luceromoreno <luceromoreno@10.18.151.6>
2016-02-24 10:37:00 -0700

    Merge origin/master
    Conflicts:
    	TheHeist/src/theheist/control/ClueControl.java
    	TheHeist/test/theheist/control/ClueControlTest.java

---
+++ TheHeist/src/byui/cit260/theHeist/view/LocationView.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHeist.view;
+
+/**
+ *
+ * @author luceromoreno
+ */
+class LocationView {
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    public boolean showChallenge() {
+        switch (location) {
+            case "Artlin":
+                //puzzle goes here
+                System.out.println("Show Artlin Puzzle");
+            case "Boury":
+                System.out.println("Show Artlin Puzzle");
+            case "Celnig":
+                System.out.println("Show Artlin Puzzle");
+            case "Duvin":
+                System.out.println("Show Artlin Puzzle");
+            case "Estrip":
+                System.out.println("Show Artlin Puzzle");
+            case "Fargin":
+                System.out.println("Show Artlin Puzzle");
+            case "Goverie":
+                System.out.println("Show Artlin Puzzle");
+            case "Howerty":
+                System.out.println("Show Artlin Puzzle");
+            case "Inlin":
+                int diamonds = 7;
+                if (diamonds == 7) {
+                    System.out.println("You got it!");
+                } 
+                else {
+                    System.out.println("Try another number");
+                }
+                
+                int rubies =5;
+                if (rubies == 5) {System.out.println("You got it!");
+                } 
+                else {
+                    System.out.println("Try another number");
+                }
+                
+                int sapphires = 3;
+                if (sapphires != 3) {
+                    System.out.println("You got it!");
+                } 
+                else {
+                    System.out.println("Try another number");
+                }
+                System.out.println("Show Artlin Puzzle");
+            case "Jawel":
+                System.out.println("Show Artlin Puzzle");
+            case "Kinte":
+                System.out.println("Show Artlin Puzzle");
+            case "Lerly":
+                System.out.println("Show Artlin Puzzle");
+            case "Murtan":
+               System.out.println("Show Artlin Puzzle");
+            case "Nolafe":
+                System.out.println("Show Artlin Puzzle");
+            case "Opstera":
+                System.out.println("Show Artlin Puzzle");
+            case "Parquin":
+               System.out.println("Show Artlin Puzzle");
+            case "Ryster":
+                System.out.println("Show Artlin Puzzle");
+            case "Swerlin":
+                System.out.println("Show Artlin Puzzle");
+            case "Terwalt":
+                System.out.println("Show Artlin Puzzle");
+            case "Urtlin":
+                System.out.println("Show Artlin Puzzle");
+            case "Verlu":
+                System.out.println("Show Artlin Puzzle");
+            case "Webel":
+                System.out.println("Show Artlin Puzzle");
+            case "Xina":
+                System.out.println("Show Artlin Puzzle");
+            case "Yaster":
+                System.out.println("Show Artlin Puzzle");
+            case "Zinta":
+                System.out.println("Show Artlin Puzzle");
+            default:
+                System.out.println("\n*** Invalid selection*** Try again");
+                break;
+        }
+
+    
+
