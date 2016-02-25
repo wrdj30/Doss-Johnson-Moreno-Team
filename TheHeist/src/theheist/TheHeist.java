75d98092f3062fdebed2c10080be66a1f3869c29
luceromoreno <luceromoreno@10.18.151.6>
2016-02-25 09:49:10 -0700

    Merge origin/master
    Conflicts:
    	TheHeist/src/byui/cit260/theHeist/view/GameMenuView.java
    	TheHeist/src/theheist/TheHeist.java
    	TheHeist/src/theheist/control/GameControl.java

--- TheHeist/src/theheist/TheHeist.java
+++ TheHeist/src/theheist/TheHeist.java
@@ -33,25 +33,6 @@ public class TheHeist {
     public static void setPlayer(Player player) {
         TheHeist.player = player;
     }
-    public static Game getCurrentGame() {
-        return currentGame;
-    }
-
-    public static void setCurrentGame(Game currentGame) {
-        TheHeist.currentGame = currentGame;
-    }
-
-    public static Player getPlayer() {
-        return player;
-    }
-
-    public static void setPlayer(Player player) {
-        TheHeist.player = player;
-    }
-    
-    private static Game currentGame = null;
-    private static Player player = null;
-
     /**
      * @param args the command line arguments
      */
