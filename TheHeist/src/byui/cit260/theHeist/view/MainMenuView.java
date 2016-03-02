cc524d2967b875054f02d545bf41cbda24d6fb2f
wr <wr@10.18.151.135>
2016-03-02 10:01:06 -0700

    MainMenuView connected to super class

--- TheHeist/src/byui/cit260/theHeist/view/MainMenuView.java
+++ TheHeist/src/byui/cit260/theHeist/view/MainMenuView.java
@@ -14,12 +14,10 @@ import theheist.control.GameControl;
  *
  * @author luceromoreno
  */
-public class MainMenuView {
-    private final String menu;
-    private String promptMessage;
+public class MainMenuView extends View{
     
     public MainMenuView() { 
-        this.menu = "\n"
+            super("\n"
                 + "\n---------------------------"
                 + "\n | Main Menu              |"
                 + "\n---------------------------"
@@ -27,39 +25,10 @@ public class MainMenuView {
                 + "\n H - Help                  "
                 + "\n L - Load game             "
                 + "\n Q - Quit                  "
-                + "\n---------------------------";
+                + "\n---------------------------");
     }
-   public void displayMainMenuView() {
-        boolean done = false;
-        do {
-            
-            String menuOption = this.getMenuOption();
-            if (menuOption.toUpperCase().equals("Q"))
-                return;
-            done = this.doAction(menuOption);
-        } while (!done);
-    }
-
-    private String getMenuOption() {
-        Scanner keyboard = new Scanner(System.in);
-        String value = "";
-        boolean valid = false;
-
-        while (!valid) {
-            System.out.println("\n" + this.menu);
-
-            value = keyboard.nextLine();
-            value = value.trim();
-
-            if (value.length() < 1) {
-                System.out.println("\nInvalid value: value can not be blank");
-                continue;
-            }
-            break;
-        }
-        return value;
-    }
-
+ 
+    @Override
     public boolean doAction(String choice) {
     
         choice = choice.toUpperCase();
