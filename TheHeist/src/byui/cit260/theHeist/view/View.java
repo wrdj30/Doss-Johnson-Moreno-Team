862bd703d3285f58b86f179e651a0989fd6b8d5b
wr <wr@10.18.151.135>
2016-03-02 10:02:44 -0700

    Made View super class. Inserted display(), getInput(), and doAction().

---
+++ TheHeist/src/byui/cit260/theHeist/view/View.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHeist.view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author wr
+ */
+public abstract class View implements ViewInterface {
+    
+    protected String promptMessage;
+    
+    public View() {        
+    }
+    
+    public View(String message) {
+        this.promptMessage = message;
+    }
+
+    @Override
+    public void display() {        
+            
+    boolean done = false;
+        do {
+            
+            String value = this.getInput();
+            if (value.toUpperCase().equals("Q"))
+                return;
+            done = this.doAction(value);
+        } while (!done);
+    }
+    
+    @Override
+    public String getInput() {
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println("\n" + this.promptMessage);
+
+            value = keyboard.nextLine();
+            value = value.trim();
+
+            if (value.length() < 1) {
+                System.out.println("\nInvalid value: value can not be blank");
+                continue;
+            }
+            break;
+        }
+        return value;    
+    }
+}
+
