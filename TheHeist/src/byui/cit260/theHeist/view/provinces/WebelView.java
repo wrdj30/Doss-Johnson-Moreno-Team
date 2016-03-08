56d2f6ab4c1de7fa0877fa672dbd7609743b7653
wr <wr@10.18.151.135>
2016-03-07 18:38:04 -0700

    New province view

---
+++ TheHeist/src/byui/cit260/theHeist/view/provinces/WebelView.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHeist.view.provinces;
+
+import byui.cit260.theHeist.view.HelpMenuView;
+import byui.cit260.theHeist.view.View;
+
+/**
+ *
+ * @author wr
+ */
+public class WebelView extends View{
+ 
+    private String menu;
+    
+    public WebelView() { 
+           super( "\n"
+                + "\n---------------------------"
+                + "\n |   Welcome to Webel     |"
+                + "\n---------------------------"
+                + "\n C - Go To Crime scene     "
+                + "\n H - Help Menu             "              
+                + "\n Q - Quit                  "
+                + "\n---------------------------");
+           
+    }
+    
+    
+    
+    @Override
+    public boolean doAction(String choice) {
+        
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "C":
+                this.goToCrimeScene();
+                break;
+            case "H":
+                this.getHelp();
+                break;            
+            case "Q":
+                this.quit();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection*** Try again");
+                break;
+        }
+        
+        return false;
+    }
+
+    private void goToCrimeScene() {
+        System.out.println("*** goToCrimeScene function called ***");
+    }
+
+    private void getHelp() {
+        HelpMenuView helpMenuView = new HelpMenuView();
+        
+            helpMenuView.display();
+    }
+        
+    private void quit() {
+        System.out.println("*** quit function called ***");
+    }
+    
+}
