b4ce70c3a70e61a02c11bd11cb439e0991d19807
wr <wr@10.18.151.135>
2016-03-09 09:44:50 -0700

    made an enum list

--- TheHeist/src/byui/cit260/theHeist/model/Actor.java
+++ TheHeist/src/byui/cit260/theHeist/model/Actor.java
@@ -5,84 +5,38 @@
  */
 package byui.cit260.theHeist.model;
 
+import java.awt.Point;
 import java.io.Serializable;
-import java.util.Objects;
+
+
 
 /**
  *
  * @author luceromoreno
  */
-public class Actor implements Serializable {
+public enum Actor implements Serializable{
     
-    private String characterName;
-    private String description;
-    private double coordinates;
-
-    public Actor() {
-    }
+    MaxZepher("The detective trying to catch the theif", new Point(1,1)),
+    Queen("The Queen of Inlin", new Point(1,1)),
+    Police("The police of Inlin", new Point(1,1));
+    
+    
+    private final String description;
+    private final Point coordinates;
 
     
-    public String getCharacterName() {
-        return characterName;
-    }
-
-    public void setCharacterName(String characterName) {
-        this.characterName = characterName;
+    
+    Actor(String description, Point coordinates) {
+        this.description = description;
+        this.coordinates =  coordinates;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getCoordinates() {
+    public Point getCoordinates() {
         return coordinates;
-    }
-
-    public void setCoordinates(double coordinates) {
-        this.coordinates = coordinates;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 23 * hash + Objects.hashCode(this.characterName);
-        hash = 23 * hash + Objects.hashCode(this.description);
-        hash = 23 * hash + (int) (Double.doubleToLongBits(this.coordinates) ^ (Double.doubleToLongBits(this.coordinates) >>> 32));
-        return hash;
-    }
-
-    @Override
-    public String toString() {
-        return "Actor{" + "characterName=" + characterName + ", description=" + description + ", coordinates=" + coordinates + '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Actor other = (Actor) obj;
-        if (Double.doubleToLongBits(this.coordinates) != Double.doubleToLongBits(other.coordinates)) {
-            return false;
-        }
-        if (!Objects.equals(this.characterName, other.characterName)) {
-            return false;
-        }
-        if (!Objects.equals(this.description, other.description)) {
-            return false;
-        }
-        return true;
-    }
-    
+    }   
     
 }
