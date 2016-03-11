f164fa60a8c7225a2eed68aa154449e2ae492c17
luceromoreno <luceromoreno@10.18.151.6>
2016-03-11 09:46:25 -0700

    03/11/16 createNewGame function

--- TheHeist/src/byui/cit260/theHeist/model/TransportationType.java
+++ TheHeist/src/byui/cit260/theHeist/model/TransportationType.java
@@ -6,70 +6,25 @@
 package byui.cit260.theHeist.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 /**
  *
  * @author wr
  */
-public class TransportationType implements Serializable {
-    
-    private String travelType;
-    private int lenghtOfTravel;
+public enum TransportationType implements Serializable {
 
-    public TransportationType() {
+    Plane("You need to use the plane if you are traveling over 200 miles"),
+    Car("Use if traveling over 8 miles"),
+    Walk("You can walk the rest of the time");
+
+    private final String description;
+
+    TransportationType(String description) {
+        this.description = description;
     }
 
-    public String getTravelType() {
-        return travelType;
+    public String getDescription() {
+        return description;
     }
 
-    public void setTravelType(String travelType) {
-        this.travelType = travelType;
-    }
-
-    public int getLenghtOfTravel() {
-        return lenghtOfTravel;
-    }
-
-    public void setLenghtOfTravel(int lenghtOfTravel) {
-        this.lenghtOfTravel = lenghtOfTravel;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 29 * hash + Objects.hashCode(this.travelType);
-        hash = 29 * hash + this.lenghtOfTravel;
-        return hash;
-    }
-
-    @Override
-    public String toString() {
-        return "TransportationType{" + "travelType=" + travelType + ", lenghtOfTravel=" + lenghtOfTravel + '}';
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final TransportationType other = (TransportationType) obj;
-        if (this.lenghtOfTravel != other.lenghtOfTravel) {
-            return false;
-        }
-        if (!Objects.equals(this.travelType, other.travelType)) {
-            return false;
-        }
-        return true;
-    }
-    
-    
-    
 }
