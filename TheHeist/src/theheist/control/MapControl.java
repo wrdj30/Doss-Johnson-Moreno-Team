747e4443a341c663d37cee936badc44894bb4d01
wr <wr@10.18.151.135>
2016-03-13 22:22:48 -0600

    Made dummy function for moveActorsToStartingLocation

--- TheHeist/src/theheist/control/MapControl.java
+++ TheHeist/src/theheist/control/MapControl.java
@@ -16,6 +16,10 @@ public class MapControl {
     public static Map createMap() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-        
+
+    static void moveActorsToStartingLocation(Map map) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
         
 }
