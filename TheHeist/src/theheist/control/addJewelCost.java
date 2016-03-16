57b0d405c04f42995b2522225224a69c35a7f0d0
luceromoreno <luceromoreno@10.18.151.6>
2016-03-15 19:18:20 -0700

    Merge origin/master
    Conflicts:
    	TheHeist/src/theheist/control/MapControl.java

---
+++ TheHeist/src/theheist/control/addJewelCost.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theheist.control;
+
+/**
+ *
+ * @author luceromoreno
+ */
+public class addJewelCost {
+   
+  public static void main(String args[]) {
+   int costs[] = new int[]{32000, 45000, 67000};
+
+   int mostExp = costs[0];
+   int leastExp = costs[0];
+   
+   for(int i = 1; i <costs.length; i++)
+   {
+       if(costs[i] > leastExp)
+           leastExp = costs[i];
+       else if(costs[i] < mostExp)
+           mostExp = costs[i];
+   }
+   System.out.println("The most expensive jewel costs: " + mostExp);
+   System.out.println("The least expensive jewel costs: " + leastExp);
+
+  }
+}
