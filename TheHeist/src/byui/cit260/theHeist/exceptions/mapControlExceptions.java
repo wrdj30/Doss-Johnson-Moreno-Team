1dfc533ccf2c2d05247d062efea9273bbd439607
wr <wr@10.18.151.135>
2016-03-16 14:43:21 -0600

    Created exceptions package and mapControlExceptions class.

---
+++ TheHeist/src/byui/cit260/theHeist/exceptions/mapControlExceptions.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.theHeist.exceptions;
+
+/**
+ *
+ * @author wr
+ */
+public class mapControlExceptions extends Exception {
+
+    public mapControlExceptions() {
+    }
+
+    public mapControlExceptions(String message) {
+        super(message);
+    }
+
+    public mapControlExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public mapControlExceptions(Throwable cause) {
+        super(cause);
+    }
+
+    public mapControlExceptions(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
